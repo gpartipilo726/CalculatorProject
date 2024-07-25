44e6628cff9d2b7eeef495b4a499e1f3caebba1e
Giuseppe Partipilo <gpartipilo726@gmail.com>
2024-07-25 15:39:22 -0400

    Update Operator logic and add power functionality - Added functionality for power calculations. - Added disabled fields to prevent multiple subsequent operators or decimals. - Added functionality to use previous calculation as the basis for the next calculation.

--- scripts.js
+++ scripts.js
@@ -8,27 +8,50 @@ const seven = document.querySelector("#calc-input7");
 const eight = document.querySelector("#calc-input8");
 const nine = document.querySelector("#calc-input9");
 const zero = document.querySelector("#calc-input0");
-const decimal = document.querySelector("#calc-input.")
+const decimal = document.querySelector("#calc-decimal");
 const add = document.querySelector("#calc-add");
 const subtract = document.querySelector("#calc-subtract");
 const multiply = document.querySelector("#calc-multiply");
 const divide = document.querySelector("#calc-divide");
+const negative = document.querySelector("#calc-negative");
+const power = document.querySelector("#calc-power");
 const equals = document.querySelector("#calc-equals");
+const clear = document.querySelector("#calc-clear");
+const allClear = document.querySelector("#calc-clearall");
 
 const input = document.querySelector("#input");
 const prevInput = document.querySelector("#prev-input");
 let values = []
+let PreviousResult;
 let operator = '';
+let reset = false;
 
-one.addEventListener("click",() =>{
+
+one.addEventListener("click",(e) =>{
+    if(reset){
+        prevInput.textContent = '';
+        input.textContent = '';
+        reset = false;
+    }
+    removeDisabled();
+
     input.textContent = 1;
     prevInput.textContent += 1;
     values.push('1');
+    console.log(values);
 
 });
 
 two.addEventListener("click",() =>{
-    input.textContent = 2;
+    console.log(reset);
+    if(reset){
+        prevInput.textContent = '';
+        input.textContent = '';
+        reset = false;
+    }
+    removeDisabled();
+    
+    input.textContent += 2;
     prevInput.textContent += 2;
     values.push('2');
     
@@ -36,87 +59,240 @@ two.addEventListener("click",() =>{
 });
 
 three.addEventListener("click",() =>{
-    input.textContent = 3;
+    if(reset){
+        prevInput.textContent = '';
+        input.textContent = '';
+        reset = false;
+    }
+    removeDisabled();
+    
+    input.textContent += 3;
     prevInput.textContent += 3;
     values.push('3');
 
 });
 
 four.addEventListener("click",() =>{
-    input.textContent = 4;
+    if(reset){
+        prevInput.textContent = '';
+        input.textContent = '';
+        reset = false;
+    }
+    removeDisabled();
+
+    input.textContent += 4;
     prevInput.textContent += 4;
     values.push('4');
 
 });
 
 five.addEventListener("click",() =>{
-    input.textContent = 5;
+    if(reset){
+        prevInput.textContent = '';
+        input.textContent = '';
+        reset = false;
+    }
+    removeDisabled();
+
+    input.textContent += 5;
     prevInput.textContent += 5;
     values.push('5');
 
 });
 
 six.addEventListener("click",() =>{
-    input.textContent = 6;
+    if(reset){
+        prevInput.textContent = '';
+        input.textContent = '';
+        reset = false;
+    }
+    removeDisabled();
+
+    input.textContent += 6;
     prevInput.textContent += 6;
     values.push('6');
 
 });
 
 seven.addEventListener("click",() =>{
-    input.textContent = 7;
+    if(reset){
+        prevInput.textContent = '';
+        input.textContent = '';
+        reset = false;
+    }
+    removeDisabled();
+
+    input.textContent += 7;
     prevInput.textContent += 7;
     values.push('7');
 
 });
 
 eight.addEventListener("click",() =>{
-    input.textContent = 8;
+    if(reset){
+        prevInput.textContent = '';
+        input.textContent = '';
+        reset = false;
+    }
+    removeDisabled();
+
+    input.textContent += 8;
     prevInput.textContent += 8;
     values.push('8');
 
 });
 
 nine.addEventListener("click",() =>{
-    input.textContent = 9;
+    if(reset){
+        prevInput.textContent = '';
+        input.textContent = '';
+        reset = false;
+    }
+    removeDisabled();
+
+    input.textContent += 9;
     prevInput.textContent += 9;
     values.push('9');
 
 });
 
 zero.addEventListener("click",() =>{
-    input.textContent = 0;
+    if(reset){
+        prevInput.textContent = '';
+        input.textContent = '';
+        reset = false;
+    }
+    removeDisabled();
+
+    input.textContent += 0;
     prevInput.textContent += 0;
     values.push('0');
 
 });
 
 decimal.addEventListener("click",() =>{
+    
     prevInput.textContent += '.';
+    input.textContent += '.';
+
     values.push('.');
+
+    if(values[values.length-1] === "."){
+        decimal.setAttribute('disabled','true');
+    } 
 });
 
 add.addEventListener("click",() =>{
-    prevInput.textContent += '+';
-    values.push('+');
+
+    if(values.length < 1){
+        reset = false;
+        prevInput.textContent = '';
+        prevInput.textContent += PreviousResult;
+        input.textContent += '+';
+        values.push(PreviousResult);
+    }
+
+    if(values[values.length-1] === '+'){
+        alert("Invalid Input. Operator already exists.")
+    } else{
+        prevInput.textContent += '+';
+        values.push('+');
+        input.textContent = '';
+
+    }
+
+    
 
 });
 
 subtract.addEventListener("click",() =>{
+
+    if(values.length < 1){
+        reset = false;
+        prevInput.textContent = '';
+        prevInput.textContent += PreviousResult;
+        input.textContent += '+';
+        values.push(PreviousResult);
+    }
+
     prevInput.textContent += '-';
     values.push('-');
+    input.textContent = '';
+
 
 });
 
 multiply.addEventListener("click",() =>{
+
+    if(values.length < 1){
+        reset = false;
+        prevInput.textContent = '';
+        prevInput.textContent += PreviousResult;
+        input.textContent += '+';
+        values.push(PreviousResult);
+    }
+
     prevInput.textContent += 'x';
     values.push('*');
+    input.textContent = '';
+
 });
 
 divide.addEventListener("click",() =>{
+
+    if(values.length < 1){
+        reset = false;
+        prevInput.textContent = '';
+        prevInput.textContent += PreviousResult;
+        input.textContent += '+';
+        values.push(PreviousResult);
+    }
+
     prevInput.textContent += '÷';
     values.push('/');
+    input.textContent = '';
+
 });
+
+
+power.addEventListener("click", () =>{
+
+    if(values.length < 1){
+        reset = false;
+        prevInput.textContent = '';
+        prevInput.textContent += PreviousResult;
+        input.textContent += '+';
+        values.push(PreviousResult);
+    }
+
+    prevInput.textContent += '^';
+    values.push('**');
+    input.textContent = '';
+});
+
+
+negative.addEventListener("click", () => {
+
+});
+
+
+
+
+clear.addEventListener("click", ()=>{
+    values.pop();
+    input.textContent = input.textContent.slice(0,input.textContent.length-1);
+    prevInput.textContent = prevInput.textContent.slice(0,prevInput.textContent.length-1);
+
+});
+
+
+allClear.addEventListener("click", ()=>{
+    values = []
+    input.textContent = '';
+    prevInput.textContent = '';
+
+});
+
 
 
 function addNum (num1, num2){
@@ -132,7 +308,21 @@ function multiplyNum (num1, num2){
 }
 
 function divideNum (num1, num2){
-    return num1/num2;
+    return Mathnum1/num2;
+}
+
+function powerNum (num1, num2){
+    return num1**num2;
+}
+
+function removeDisabled(){
+    decimal.removeAttribute('disabled','false');
+    subtract.removeAttribute('disabled','false');
+    add.removeAttribute('disabled','false');
+    multiply.removeAttribute('disabled','false');
+    divide.removeAttribute('disabled','false');
+    equals.removeAttribute('disabled','false');
+
 }
 
 function Operate (num1, num2, operator){
@@ -144,11 +334,14 @@ function Operate (num1, num2, operator){
         case '-':
             results = subtractNum(num1, num2);
             break;
-        case 'x':
+        case '*':
             results = multiplyNum(num1, num2);
             break;
-        case '÷':
+        case '/':
             results = divideNum(num1, num2);
+            break;
+        case '**':
+            results = powerNum(num1, num2);
             break;
     }
     return results;
@@ -157,20 +350,23 @@ function Operate (num1, num2, operator){
 
 function findOperator(array){
 
-    return array.filter(operator => operator === '+' || operator === '-' || operator === 'x' || operator === '÷').toString();
+    return array.filter(operator => operator === '+' || operator === '-' || operator === '*' || operator === '/' || operator === '**').toString();
 }
 
 equals.addEventListener("click",()=>{
     prevInput.textContent += '=';
-    console.log(values);
     let operator = findOperator(values);
     let OperatorIndex = values.indexOf(operator);
     let num1 = parseFloat(values.slice(0, OperatorIndex).join(''));
     let num2 = parseFloat(values.slice(OperatorIndex+1, values.length).join(''));
-    console.log(num1);
-    console.log(typeof(num1));
-    console.log(num2);
-    console.log(typeof(num2));
+
+    result = Operate(num1, num2, operator);
+    input.textContent = result;
+    PreviousResult = result;
+    values = [];
+    reset = true;
+
+
 });
 
 
